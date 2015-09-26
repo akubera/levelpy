c254fb9e405ea0a819871e989be06b99ba32a4db
Andrew Kubera <andrewkubera@gmail.com>
2015-09-26 02:51:10 -0400

    leveldb: Constructor reimplemented to provide more robust dependency injection. Tests break

--- levelpy/leveldb.py
+++ levelpy/leveldb.py
@@ -7,36 +7,77 @@ from .batch_context import BatchContext
 
 class LevelDB:
     """
-    LevelDB interface
+    LevelDB interface.
+
+    This is the wrapper class around a 'real' implementation of LevelDB.
     """
 
     _db = None
-    leveldb = None
+    _leveldb_cls = None
+    _leveldb_pkg = None
     path = None
 
     def __init__(self,
                  db,
-                 database_package='leveldb',
+                 leveldb_cls='leveldb.LevelDB',
                  **kwargs):
-        if isinstance(database_package, str):
-            self.leveldb = __import__(database_package)
-        else:
-            self.leveldb = database_package
-
+        # if db is a string - create the db object from the leveldb_cls param
         if isinstance(db, str):
-            self.path = db
-            db = self._db = self.leveldb.LevelDB(self.path, **kwargs)
+
+            # injected class name by string
+            if isinstance(leveldb_cls, str):
+                # everything before last dot is package, after is class name
+                last_dot = leveldb_cls.rfind('.')
+                pkg = leveldb_cls[:last_dot]
+                db_classname = leveldb_cls[last_dot+1:]
+                self._leveldb_pkg = __import__(pkg)
+                self._leveldb_cls = getattr(self._leveldb_pkg, db_classname)
+
+            # passed the class directly
+            elif isinstance(leveldb_cls, type):
+                self._leveldb_pkg = leveldb_cls.__modue__
+                self._leveldb_cls = leveldb_cls
+
+            # leveldb_cls *should* provide the backend
+            else:
+                self._leveldb_pkg = None
+                self._leveldb_cls = leveldb_cls
+
+            # create the backend
+            self._db = self._leveldb_cls(db, **kwargs)
+
+            # The backend package was not determined.
+            # Provided 'class' was just a factory function - inspect
+            # resultant object to get real class and package names
+            if self._leveldb_pkg is None:
+                self._leveldb_cls = self._db.__class__
+                self._leveldb_pkg = self._leveldb_cls.__module__
+
+        # db was an object - just copy information
         else:
             self._db = db
+            self._leveldb_cls = self._db.__class__
+            self._leveldb_pkg = self._leveldb_cls.__module__
+
+        def copy_attr(name, items):
+            for item in items:
+                if hasattr(self._db, item):
+                    setattr(self, name, item)
+                    return
+            setattr(self, name, None)
 
         # Alias methods
-        self.Put = self._db.Put
-        self.Get = self._db.Get
-        self.Delete = self._db.Delete
-        self.Write = self._db.Write
-        self.RangeIter = self._db.RangeIter
-        self.GetStats = self._db.GetStats
-        self.CreateSnapshot = self._db.CreateSnapshot
+        copy_attr('Put', ('Put', 'put'))
+        copy_attr('Get', ('Get', 'get'))
+        copy_attr('Delete', ('Delete', 'delete'))
+        copy_attr('Write', ('Write', 'write'))
+        copy_attr('RangeIter', ('RangeIter', 'range_iter'))
+        copy_attr('GetStats', ('GetStats', 'get_stats'))
+        copy_attr('CreateSnapshot', ('CreateSnapshot', 'snapshot'))
+        copy_attr('WriteBatch', ('WriteBatch', 'write_batch'))
+
+        # attempt to copy the path of the database
+        copy_attr('path', ('name', 'filename'))
 
     def __getitem__(self, key):
         if isinstance(key, slice):
