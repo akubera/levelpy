44a6ef25d42e5ab477f93351c1d7a857fe413583
Andrew Kubera <andrewkubera@gmail.com>
2015-11-13 20:43:10 -0500

    (test) Added iterview tests to test_real_db; 100% COVERAGE! 🎉🎉

--- tests/test_real_db.py
+++ tests/test_real_db.py
@@ -48,6 +48,13 @@ def fresh_db(cleared_database):
     return LevelDB(cleared_database)
 
 
+@pytest.fixture
+def filled_db(db, data):
+    for k, v in data:
+        db[k] = v
+    return db
+
+
 def test_backend_class(backend_class, leveldir):
     db = backend_class(leveldir)
     assert isinstance(db, backend_class)
@@ -133,38 +140,83 @@ def test_item_iteration_slice(db, slice_, data, expected):
     assert len(l) is len(expected)
     assert l == expected
 
-@pytest.mark.parametrize('slice_, data, expected', (
-    ( (b'A', b'B'),
-      [('A0', 'a'), ('A1', 'b'), ('C', 'c')],
-      ('a', 'b')
+@pytest.mark.parametrize('data, range_, expected', (
+    ([('A0', 'a'), ('A1', 'b'), ('C', 'c')],
+      ('a', 'b'),
+      [(b'A0', 'a'), (b'A1', 'b'), (b'C', 'c')]
     ),
-    ( ('A', 'B'),
-      [('A0', 'a'), ('A1', 'b'), ('C', 'c')],
-      ('a', 'b')
+))
+def test_items(filled_db, range_, expected):
+    db = filled_db
+    items = db.items()
+    for a, b in zip(items, expected):
+        assert a == b
+
+
+@pytest.mark.parametrize('data, range_, expected', (
+    ([('A0', 'a'), ('A1', 'b'), ('C', 'c')],
+      ('a', 'b'),
+      [(b'C', 'c'), (b'A1', 'b'), (b'A0', 'a')]
     ),
-    ( ('A', None),
-      [('A0', 'a'), ('A1', 'b'), ('C', 'c')],
-      ('a', 'b', 'c')
+))
+def test_items_reversed(filled_db, range_, expected):
+    db = filled_db
+    items = db.items()
+    for a, b in zip(reversed(items), expected):
+        assert a == b
+
+
+@pytest.mark.parametrize('data, range_, expected', (
+    ([('A0', 'a'), ('A1', 'b'), ('C', 'c')],
+      ('a', 'b'),
+      [b'A0', b'A1', b'C']
     ),
-    ( ('A1', None),
-      [('A0', 'a'), ('A1', 'b'), ('C', 'c')],
-      ('b', 'c')
+))
+def test_keys(filled_db, range_, expected):
+    db = filled_db
+    keys = db.keys()
+    for a, b in zip(keys, expected):
+        assert a == b
+
+
+@pytest.mark.parametrize('data, range_, expected', (
+    ([('A0', 'a'), ('A1', 'b'), ('C', 'c')],
+      ('a', 'b'),
+      [b'C', b'A1', b'A0']
     ),
-    ( (None, 'A1'),
-      [('A0', 'a'), ('A1', 'b'), ('C', 'c')],
-      ('a', 'b')
-    ),
-    ( (None, None),
-      [('A0', 'a'), ('A1', 'b'), ('C', 'c')],
+))
+def test_keys_reversed(filled_db, range_, expected):
+    db = filled_db
+    keys = db.keys()
+    for a, b in zip(reversed(keys), expected):
+        assert a == b
+
+
+@pytest.mark.parametrize('data, range_, expected', (
+    ([('A0', 'a'), ('A1', 'b'), ('C', 'c')],
+      ('a', 'b'),
       ['a', 'b', 'c']
     ),
 ))
-def test_items(db, slice_, data, expected):
+def test_values(filled_db, range_, expected):
+    db = filled_db
+    vals = db.values()
+    for a, b in zip(vals, expected):
+        assert a == b
 
-    for k, v in data:
-        db[k] = v
 
-    l = type(expected)(v.decode() for k, v in db[slice_[0]:slice_[1]])
+@pytest.mark.parametrize('data, range_, expected', (
+    ([('A0', 'a'), ('A1', 'b'), ('C', 'c')],
+      ('a', 'b'),
+      ['c', 'b', 'a']
+    ),
+))
+def test_values_reversed(filled_db, range_, expected):
+    db = filled_db
+    vals = db.values()
+    for a, b in zip(reversed(vals), expected):
+        assert a == b
 
-    assert len(l) is len(expected)
-    assert l == expected
+def test_tostring(db):
+    vals = db.values()
+    str(vals) == "<LevelValues>"
